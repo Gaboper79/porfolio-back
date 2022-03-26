5ddcd46f5a48d6f11aa9acada12ca8a51724c8a7
Gaboper79 <gp130579@gmail.com>
2022-03-26 16:24:48 -0300

    Inicial

---
+++ src/main/java/com/backporfolio/BackPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.backporfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPortfolioApplication.class, args);
+	}
+
+}
