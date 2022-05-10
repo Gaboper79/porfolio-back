a88ce8a1507c25aed71073866fa7175c62b3abd1
Gaboper79 <gp130579@gmail.com>
2022-05-10 19:33:29 -0300

    cors

--- src/main/java/com/backporfolio/BackPortfolioApplication.java
+++ src/main/java/com/backporfolio/BackPortfolioApplication.java
@@ -2,6 +2,7 @@ package com.backporfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,7 @@ public class BackPortfolioApplication {
 		SpringApplication.run(BackPortfolioApplication.class, args);
 	}
 
+	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
