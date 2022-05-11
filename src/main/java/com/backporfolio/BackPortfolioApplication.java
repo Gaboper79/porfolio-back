62a44a22770151af56983276d30a9255fcfe8ca2
Gaboper79 <gp130579@gmail.com>
2022-05-11 09:56:39 -0300

    cors

--- src/main/java/com/backporfolio/BackPortfolioApplication.java
+++ src/main/java/com/backporfolio/BackPortfolioApplication.java
@@ -18,7 +18,7 @@ public class BackPortfolioApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("*");
+				registry.addMapping("*").allowedOrigins("*");
 			}
 		};
 	}
